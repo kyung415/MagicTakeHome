bcb2ec2e28db9eeafea38a32d376724259420650
kyung415 <kjlee415@gmail.com>
2020-10-23 11:53:49 -0700

    Added Search feature css.

--- magictakehome/src/Search/SearchRow.jsx
+++ magictakehome/src/Search/SearchRow.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 
+import { Button } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css';
+import './SearchRow.css'
 /*
     Search Bar searched results row.
     Shows Location title and an + icon button.
 */
 const SearchRow = (props) => {
     return (
-        <div>
-            <div>{props.location}</div>
-            <button>+</button>
+        <div className='searchRowContainer'>
+            <div id='searchRowTitle'>{props.location}</div>
+            <div id='searchRowButtonContainer'>
+                <Button icon='plus' size='mini' color='blue'></Button>
+            </div>
         </div>
     )
 }
